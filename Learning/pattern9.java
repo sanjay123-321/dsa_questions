11e5803c7e86105f9a5208da550784d3ab0a1455
sanjay123-321 <sanjayup21@gmail.com>
2025-05-05 17:31:12 -0400

    Pattern questions done

---
+++ Learning/pattern9.java
@@ -0,0 +1,25 @@
+package Learning;
+import java.util.Scanner;
+//         1
+//       2 1 2
+//     3 2 1 2 3
+//   4 3 2 1 2 3 4
+// 5 4 3 2 1 2 3 4 5
+public class pattern9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<= n-i;j++){
+                System.out.print("  ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j+" ");
+            }
+            for(int j=2;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
